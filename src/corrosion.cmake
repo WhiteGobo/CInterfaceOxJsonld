1aa3b9c9d2e835d77f9433b86b7cc9e5f24869da
Focke <richardfechner@posteo.net>
2026-05-08 11:30:33 +0200

    added find_PACKAGE_ARGS to force find_package for Corrosion

--- src/corrosion.cmake
+++ src/corrosion.cmake
@@ -1,8 +1,9 @@
 include(FetchContent)
 
 FetchContent_Declare(
-    Corrosion
-    GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
-    GIT_TAG v0.6 # Optionally specify a commit hash, version tag or branch here
+	Corrosion
+	GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
+	GIT_TAG v0.6 # Optionally specify a commit hash, version tag or branch here
+	FIND_PACKAGE_ARGS
 )
 FetchContent_MakeAvailable(Corrosion)
